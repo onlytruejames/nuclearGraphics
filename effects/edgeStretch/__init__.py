210e36c595bb3dbb3cb8b9e52beea2bcd663d043
James Young <james@chaosgb.co.uk>
2024-08-08 21:55:58 +0100

    made edgeStretch a bit better

--- effects/edgeStretch/__init__.py
+++ effects/edgeStretch/__init__.py
@@ -1,15 +1,18 @@
 from PIL import Image
 import numpy as np
-from random import randint, choice
+from random import randint, choice, shuffle
 
 getDecay = lambda: int((randint(20, 40) / 10) ** 2)
-getDir = lambda: int((randint(10, 25) / 10) ** 2) * choice([-1, 1])
+def getDir():
+    dir = [int((randint(17, 25) / 17) ** 2) * choice([-1, 1]), int((randint(0, 25) / 17) ** 2) * choice([-1, 1])]
+    shuffle(dir)
+    return tuple(dir)
 
 def variables(dims, clb):
     global lastImg, baseImg, direction, decay
     lastImg = Image.new("RGBA", dims, (0, 0, 0, 0))
     baseImg = Image.new("RGBA", dims, (0, 0, 0, 0))
-    direction = (getDir(), getDir())
+    direction = getDir()
     decay = getDecay()
 
 def changeDims(dims):
@@ -21,10 +24,12 @@ def callback(img):
     global lastImg, direction, decay
     decay -= 1
     if decay <= 0:
-        direction = (getDir(), getDir())
+        direction = getDir()
         decay = getDecay()
     lastImg = np.array(lastImg)
     lastImg = np.roll(lastImg, direction, (0, 1))
+    lastImg = lastImg.astype(np.int16) - 5
+    lastImg = np.clip(lastImg, 0, 255).astype(np.uint8)
     lastImg = Image.fromarray(lastImg)
     lastImg = Image.alpha_composite(lastImg, img)
     return lastImg
